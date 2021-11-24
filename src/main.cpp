a07cd542467610ebcfca920f9ae4e36ea76a3e74
DinoMax00 <527663226@qq.com>
2021-11-24 22:24:35 +0800

    clean repo

--- src/main.cpp
+++ src/main.cpp
@@ -32,25 +32,32 @@ bool firstMouse = true;
 float deltaTime = 0.0f;	// time between current frame and last frame
 float lastFrame = 0.0f;
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
-
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		camera.ProcessKeyboard(FORWARD, 20*deltaTime);
+		camera.ProcessKeyboard(FORWARD, deltaTime);
 	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		camera.ProcessKeyboard(BACKWARD, 20 * deltaTime);
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
 	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		camera.ProcessKeyboard(LEFT, 20 * deltaTime);
+		camera.ProcessKeyboard(LEFT, deltaTime);
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		camera.ProcessKeyboard(RIGHT, 20 * deltaTime);
+		camera.ProcessKeyboard(RIGHT, deltaTime);
 }
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
 
 // glfw: whenever the mouse moves, this callback is called
 // -------------------------------------------------------
@@ -92,41 +99,45 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Low-Poly-World", NULL, NULL);
+	
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
+
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glfwSetCursorPosCallback(window, mouse_callback);
 	glfwSetScrollCallback(window, scroll_callback);
+
 	// tell GLFW to capture our mouse
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
+
 	glEnable(GL_DEPTH_TEST);
-	Shader ourShader("test.vs", "test.fs");
+
+	Shader ourShader("shaders/test.vs", "shaders/test.fs");
 
 	ColourGenerator colourGen(TERRAIN_COLS,5);
 	TerrainGenerator specialGenerator(colourGen);
 	specialGenerator.generateTerrain(SIZE);
-for (int i = 0; i < map.indicesIndex; i += 3)
-	{
-		std::cout << "x=" << map.terrainIndices[i] << " y=" << map.terrainIndices[i + 1] << " z=" << map.terrainIndices[i + 2] << "\n";
-	}
+
 	specialGenerator.TerrainRender();
 	
-
 	//光照位置必须远大于地图大小才有明显颜色且反向比正向亮，未知原因（是否是法向量计算错误？目前使用加强环境光照看清
 	glm::vec3 lightPos((float)SIZE/2.0, (float)SIZE / 2.0, (float)SIZE *10.0);
 	glm::vec3 lightColour(1.0f, 1.0f, 1.0f);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		// per-frame time logic
@@ -141,6 +152,7 @@ for (int i = 0; i < map.indicesIndex; i += 3)
 
 		specialGenerator.TerrainUseshader(ourShader, camera, lightPos, lightColour);
 		specialGenerator.TerrainDraw();
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
